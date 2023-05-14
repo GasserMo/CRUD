dc85499776de24e6994d625408c81fe8f1f6f9df
GasserMo <133467329+GasserMo@users.noreply.github.com>
2023-05-14 21:36:22 +0300

    Add files via upload

---
+++ gasser.js
@@ -0,0 +1,61 @@
+const express =require('express');
+const bodyParser=require('body-parser');
+const app = express()
+app.use(bodyParser.json())
+app.use(express.static("gasser2"));
+
+let persons = [
+    { id: 1,
+       name: 'gasser',
+       age: 22,
+       gender: 'male',
+       email: 'gasser22@example.com' }
+  ]
+  
+  app.get('/persons', (req, res) => {
+    res.json(persons)
+  })
+  
+  app.post('/persons', (req, res) => {
+    const person = req.body
+    person.id = persons.length + 1
+    persons.push(person)
+    res.json(person)
+  })
+  
+  app.get('/persons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const person = persons.find(p => p.id === id)
+    if (person) {
+      res.json(person)
+    } else {
+      res.status(404).send('Person not found')
+    }
+  })
+  
+  app.put('/persons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const personIndex = persons.findIndex(p => p.id === id)
+    if (personIndex !== -1) {
+      const person = req.body
+      person.id = id
+      persons[personIndex] = person
+      res.json(person)
+    } else {
+      res.status(404).send('Person not found')
+    }
+  })
+  
+  app.delete('/persons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const personIndex = persons.findIndex(p => p.id === id)
+    if (personIndex !== -1) {
+      persons.splice(personIndex, 1)
+      res.sendStatus(204)
+    } else {
+      res.status(404).send('Person not found')
+    }
+  })
+  app.listen(4000, () => {
+    console.log('Server running on port 4000')
+  })
